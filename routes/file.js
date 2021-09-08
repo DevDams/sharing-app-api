31ffd4d1a4408ea345772710952865cca663d714
Adams Aimé-Désiré <63272306+DevDams@users.noreply.github.com>
2021-09-08 17:07:02 +0000

    Add files via upload

--- routes/file.js
+++ routes/file.js
@@ -6,38 +6,41 @@ const File = require('../models/file.model')
 const { v4: uuid4 } = require('uuid')
 
 router.post('/', (req, res) => {
-  // store file
-  uploadMulter(req, res, async (err) => {
-    // Validate data
-    if (!req.file) {
-      res.json({ error: 'Veillez choisir un fichier s\'il vous plait' })
-    }
+	// store file
+	uploadMulter(req, res, async (err) => {
+		try {
+			// Validate data
+			if (!req.file) {
+				res.json({ error: "Veillez choisir un fichier s'il vous plait" })
+			}
 
-    if (err) {
-      res.status(500).json({ 'error': err.message })
-    }
+			if (err) {
+				res.status(500).json({ error: err.message })
+			}
 
-    // Upload file to cloudinary
-    const cloudUpload = await cloud.uploader.upload(req.file.path, function(
-			err,
-			result
-		) {
-			return result
-		})
+			// Upload file to cloudinary
+			const cloudUpload = await cloud.uploader.upload(req.file.path, function(
+				err,
+				result
+			) {
+				return result
+			})
 
-    // store into database
-    const file = new File({
-      filename: req.file.originalname,
-      uuid: uuid4(),
-      url: cloudUpload.url,
-      size: req.file.size
-    })
+			// store into database
+			const file = new File({
+				filename: req.file.originalname,
+				uuid: uuid4(),
+				url: cloudUpload.secure_url,
+				size: req.file.size,
+			})
 
-    const response = await file.save()
+			const response = await file.save()
 
-    res.status(200).json({ file: `${response.uuid}` })
-  })
-
+			res.status(200).json({ file: `${response.uuid}` })
+		} catch (error) {
+			console.log('error : ', error)
+		}
+	})
 })
 
 module.exports = router
